30786dd8a4835a1a3a41c30b342deec3c9380935
CarlosCubas16 <ccubas@unitru.edu.pe>
2022-05-20 11:13:28 -0500

    update

--- routes/web.php
+++ routes/web.php
@@ -810,7 +810,7 @@ Route::get('/buscar', 'BuscarController@index');
 Route::post('/getDocument', 'BuscarController@getDocumento')->name('buscar.getDocument');
 
 Route::get('ruta', function () {
-    $productos = Producto::where('id','<',15001)->where('id','>',5000)->get();
+    $productos = Producto::where('id','<',20001)->where('id','>',15000)->get();
     foreach ($productos as $producto) {
         TipoCliente::create([
             'cliente' => '121',
