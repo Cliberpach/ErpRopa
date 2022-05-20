74120c090fe6f5ccc91ce7b4d7d0a48280282034
CarlosCubas16 <ccubas@unitru.edu.pe>
2022-05-20 15:51:44 -0500

    update

--- app/Http/Controllers/Almacenes/ProductoController.php
+++ app/Http/Controllers/Almacenes/ProductoController.php
@@ -53,6 +53,7 @@ class ProductoController extends Controller
             ->join('categorias', 'categorias.id', '=', 'productos.categoria_id')
             ->join('marcas', 'marcas.id', '=', 'productos.marca_id')
             ->join('almacenes', 'almacenes.id', '=', 'productos.almacen_id')
+            ->join('temporada', 'temporada.id', '=', 'productos.temporada_id')
             ->join('genero', 'genero.id', '=', 'productos.genero_id')
             ->select('categorias.descripcion as categoria', 'almacenes.descripcion as almacen', 'marcas.marca', 'productos.*')
             ->orderBy('productos.id', 'DESC')
